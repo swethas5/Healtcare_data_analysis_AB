b3483c8276e2868048b4ca7214a22f9a39090697
Swetha S <sweinbiotech@gmail.com>
2026-07-21 15:39:42 -0700

    Update Queries.sql

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -4,9 +4,9 @@
 
 with claim_summary as
 (select claim_type, count(*) as number_of_claims, sum(paid_amount) as total_paid_amount,
-sum(billed_amount) as total_billed_amount
+sum(billed_amount) as total_billed_amount,dense_rank() over(order by sum(paid_amount) desc) as Ranking
 from claims
 group by claim_type)
 select claim_type, number_of_claims, total_paid_amount,
-total_billed_amount ,dense_rank() over(order by total_paid_amount desc) as Ranking
+total_billed_amount
 from claim_summary;
